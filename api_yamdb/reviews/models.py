f89cb061a87586a3dcb255774d818012af7e41d2
Гусельников Денис <dguselnikov318@gmail.com>
2022-08-23 20:00:47 +0300

    Project: reviews/models: category, genre, title

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -8,3 +8,41 @@ class Review(models.Model):
 
 class Comments(models.Model):
     pass
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Категория')
+    slug = models.SlugField(unique=True, verbose_name='Слаг категории')
+
+    def __str__(self):
+        return self.title
+
+
+class Genre(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Жанр')
+    slug = models.SlugField(unique=True, verbose_name='Слаг жанра')
+
+    def __str__(self):
+        return self.title
+
+
+class Title(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Произведение')
+    description = models.TextField(verbose_name='Описание')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name='Категория',
+        related_name='category'        
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        verbose_name='Жанр',
+        related_name='genre'
+    )
+
+    def __str__(self):
+        return self.title
+
