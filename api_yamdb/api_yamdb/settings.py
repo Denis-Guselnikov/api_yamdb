446dfa90fa397f9402cd2e34ce32aa6bd4e862e3
Гусельников Денис <dguselnikov318@gmail.com>
2022-08-27 19:40:42 +0300

    bug/fix

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -19,11 +19,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_filters',
     'rest_framework',
-    'rest_framework_simplejwt',
-    'reviews',
+    'django_filters',
     'api',
+    'reviews',
 ]
 
 MIDDLEWARE = [
